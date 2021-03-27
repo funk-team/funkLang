58a6f5ad7cd694da6d9e980f38e17e710ef37915
Jeroen Engels <jfm.engels@gmail.com>
2021-03-27 12:13:06 +0100

    Enable elm-review

---
+++ review/src/ReviewConfig.elm
@@ -0,0 +1,30 @@
+module ReviewConfig exposing (config)
+
+{-| Do not rename the ReviewConfig module or the config function, because
+`elm-review` will look for these.
+
+To add packages that contain rules, add them to this review project using
+
+    `elm install author/packagename`
+
+when inside the directory containing this file.
+
+-}
+
+import NoUnused.Variables
+import Review.Rule as Rule exposing (Rule)
+
+
+config : List Rule
+config =
+    [ NoUnused.Variables.rule
+
+    --, NoUnused.CustomTypeConstructors.rule []
+    --, NoUnused.CustomTypeConstructorArgs.rule
+    --, NoUnused.Dependencies.rule
+    --, NoUnused.Exports.rule
+    --, NoUnused.Modules.rule
+    --, NoUnused.Parameters.rule
+    --, NoUnused.Patterns.rule
+    ]
+        |> List.map (Rule.ignoreErrorsForFiles [ "src/Viewport.elm", "src/Ui/Boxicons", "src/Ui/EntypoIcons.elm" ])
